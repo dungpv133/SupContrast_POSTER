f1dc771d49e6f45e09a1eb8c7c7542f53ee1d716
Viet Dung <haconga1903@gmail.com>
2023-09-11 23:40:35 +0700

    Update emotion_hyp.py

--- models/emotion_hyp.py
+++ models/emotion_hyp.py
@@ -85,9 +85,9 @@ class pyramid_trans_expr(nn.Module):
 
         self.face_landback = MobileFaceNet([112, 112],136)
         #running on colab
-        # face_landback_checkpoint = torch.load('./models/pretrain/mobilefacenet_model_best.pth.tar', map_location=lambda storage, loc: storage)
+        face_landback_checkpoint = torch.load('./models/pretrain/mobilefacenet_model_best.pth.tar', map_location=lambda storage, loc: storage)
         #running on kaggle
-        face_landback_checkpoint = torch.load('/kaggle/input/poster-adaface-pretrain/pretrain/pretrain/mobilefacenet_model_best.pth.tar', map_location=lambda storage, loc: storage)
+        # face_landback_checkpoint = torch.load('/kaggle/input/poster-adaface-pretrain/pretrain/pretrain/mobilefacenet_model_best.pth.tar', map_location=lambda storage, loc: storage)
         self.face_landback.load_state_dict(face_landback_checkpoint['state_dict'])
 
 
@@ -99,9 +99,9 @@ class pyramid_trans_expr(nn.Module):
 
         self.ir_back = Backbone(50, 0.0, 'ir')
         #running on colab
-        # ir_checkpoint = torch.load('./models/pretrain/ir50.pth', map_location=lambda storage, loc: storage)
+        ir_checkpoint = torch.load('./models/pretrain/ir50.pth', map_location=lambda storage, loc: storage)
         #running on kaggle
-        ir_checkpoint = torch.load('/kaggle/input/poster-adaface-pretrain/pretrain/pretrain/ir50.pth', map_location=lambda storage, loc: storage)
+        # ir_checkpoint = torch.load('/kaggle/input/poster-adaface-pretrain/pretrain/pretrain/ir50.pth', map_location=lambda storage, loc: storage)
         # ir_checkpoint = ir_checkpoint["model"]
         self.ir_back = load_pretrained_weights(self.ir_back, ir_checkpoint)
 
